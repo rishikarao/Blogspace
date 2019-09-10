cadba772d98b9c63dc4270cc7e4732c2d7275262
Rishika Rao <rishikakanakandula@gmail.com>
2019-09-10 20:48:38 +0530

    updated static root

--- blogspace/config.py
+++ blogspace/config.py
@@ -1,10 +1,9 @@
 import os
 class Config:
 
-	STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-	STATIC_URL='/static/'
 	SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+
 	MAIL_SERVER='smtp.googlemail.com'
 	MAIL_PORT=587
 	MAIL_USE_TLS=True
